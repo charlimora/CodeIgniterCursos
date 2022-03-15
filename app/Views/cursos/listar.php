aa9aba3c4e5de1ef96015bfa37b1c7c52d4888cb
charlimora <ca.agudelo.2021@gmail.com>
2022-03-15 18:35:31 -0500

    añadiendo guia tutorial y vista listar

--- app/Views/cursos/listar.php
+++ app/Views/cursos/listar.php
@@ -4,8 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document Title</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <h2>Listado de cursos</h2>
+    <div class="container">
+        <table class="table table-dark">
+            <thead class="thead-light">
+                <tr>
+                    <th>#</th>
+                    <th>Imagen</th>
+                    <th>Nombre</th>
+                    <th>Acciones</th> 
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
