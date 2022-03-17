47fee83ac0ac6ee64b60d0f85acf7ac5ddedff18
charlimora <ca.agudelo.2021@gmail.com>
2022-03-17 14:07:22 -0500

    Modificaciones del 16 de marzo 2022

--- app/Controllers/Cursos.php
+++ app/Controllers/Cursos.php
@@ -6,6 +6,17 @@ use App\Models\Curso;
 class Cursos extends Controller{
 
     public function index(){
-        return view('cursos/listar');
+
+        //se crea el objeto de clase Curso
+        $curso = new Curso();
+        /*se crea un array e indico que se orden la información por id y en forma ascedente
+        y que se busquen todos los registros. Todo se va a depositar en $datos[]*/
+        $datos['cursos'] = $curso->orderBy('id', 'ASC')->findAll();
+        /*llamo a la cabecera y al pie de página desde el controlador y asignándolo también a datos
+        recordar que $datos es un array pero almacenamos en sus diferentes
+        posiciones indexadas con los nombres cursos, cabecera y pie*/
+        $datos['cabecera'] = view('template/cabecera');
+        $datos['pie'] = view('template/piepagina');
+        return view('cursos/listar',$datos);
     }
 }
--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -6,5 +6,7 @@ use CodeIgniter\Model;
 class Curso extends Model{
     protected $table      = 'cursos';
     // Uncomment below if you want add primary key
-    // protected $primaryKey = 'id';
+    protected $primaryKey = 'id';
+    //activar el acceso a las columnas
+    protected $allowedFields = ['nombre','imagen'];
 }
